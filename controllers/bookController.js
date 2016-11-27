8690209ab2a911d53a42c1f694ccb50e93b53483
Chris Guidry <cfguidry@gmail.com>
2016-11-26 21:38:18 -0500

    Improved handling of search term session storage. Changed book title to be bold.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,5 @@
-angular.module('bookController', [])
-    .controller('bookController', ['$scope', '$location', '$http','Books', function($scope, $location, $http, Books) {
+angular.module("bookController", [])
+    .controller("bookController", ["$scope", "$location", "$http","Books", function($scope, $location, $http, Books) {
         $scope.formData = {};
         $scope.loading = false;                
         $scope.loaded = false;
@@ -7,7 +7,7 @@ angular.module('bookController', [])
         $scope.bookSearch = function() {
             $scope.books = {};
             $scope.loading = true;
-            searchTerm = encodeURIComponent($scope.formData.searchTerm);
+            searchTerm = '"' + encodeURIComponent($scope.formData.searchTerm) + '"';
             searchTerms = [];
             Books.get(searchTerm)
                 .success(function(data) {
@@ -18,8 +18,8 @@ angular.module('bookController', [])
             if(sessionStorage.searchTerms) {
                 searchTerms = sessionStorage.getItem("searchTerms").split(",");
             }
-            searchTerms.push($scope.formData.searchTerm);
-            sessionStorage.setItem("searchTerms", searchTerms);
+            searchTerms.push(searchTerm);
+            sessionStorage.setItem("searchTerms", (searchTerms));
             //Browser history - not implemented for now, but adding this line plus a bit more code would allow the browser back button
             //to be used to go back to previous searches
             //$location.url("?term=" + searchTerm);
@@ -27,15 +27,15 @@ angular.module('bookController', [])
         $scope.authorClick = function(author) {
             $scope.formData.searchTerm = author;
             $scope.bookSearch();
-        }
+        };
     }])
-    .directive('focus', function($timeout) {
+    .directive("focus", function($timeout) {
         return {
             scope : {
-                trigger : '@focus'
+                trigger : "@focus"
             },
             link : function(scope, element) {
-                scope.$watch('trigger', function(value) {
+                scope.$watch("trigger", function(value) {
                     if (value === "true") {
                         $timeout(function() {
                             element[0].focus();
