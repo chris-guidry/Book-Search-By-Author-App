5c883f790d5324a34c607684a8fa974037fe71fc
Chris Guidry <cfguidry@gmail.com>
2016-11-27 21:12:29 -0500

    Changed call to API to use partial request so that only the needed fields are returned.

--- services/bookService.js
+++ services/bookService.js
@@ -2,7 +2,7 @@ angular.module("bookService", [])
     .factory("Books", ["$http",function($http) {
         return {
             get : function(searchTerm) {
-                return $http.get("https://www.googleapis.com/books/v1/volumes?maxResults=10&q=inauthor:" + searchTerm);
+                return $http.get("https://www.googleapis.com/books/v1/volumes?maxResults=10&fields=items(volumeInfo(title,authors,publishedDate,description,imageLinks,previewLink))&q=inauthor:" + searchTerm);
             }
         };
     }]);
