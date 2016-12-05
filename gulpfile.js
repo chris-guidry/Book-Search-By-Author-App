e39cb0cbcfb3224a2fe3e77651635604df0b02ba
Chris Guidry <cfguidry@gmail.com>
2016-12-05 11:09:42 -0500

    Added gulp webserver and set to run when gulp is started (after JS/CSS is minified).

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
+var webserver = require("gulp-webserver");
 
 gulp.task("sass", function () {
     return gulp.src("./sass/**/*.scss")
@@ -16,8 +17,18 @@ gulp.task("js", function () {
         .pipe(gulp.dest("js/"));
 });
 
+gulp.task("webserver", function() {
+    gulp.src("./")
+        .pipe(webserver({
+            livereload: true,
+            open: true,
+            fallback: "index.html"
+        }));
+});
+
 //Watch task
 gulp.task("default",function() {
     gulp.watch("sass/**/*.scss",["sass"]);
     gulp.watch(["js/app.js", "services/*.js", "controllers/*.js"], ["js"]);
+    return gulp.start(["webserver"]);
 });
